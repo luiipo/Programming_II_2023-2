50121b11bd41aa056c934202628265f3f758da8e
luiipo <144996713+luiipo@users.noreply.github.com>
2023-09-14 16:53:21 +0900

    Add files via upload

---
+++ 006_do_While/006_do_While.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int i = 1;
+	do
+	{
+		printf("%d ", i);
+		i++;
+	} while (i <= 10);
+
+	//0을 입력할 때까지 입력 받은 수를 출력하는 프로그램
+	int n;
+	
+	do
+	{
+		printf("0을 입력하면 종료\n");
+		scanf_s("%d", &n);
+	} while (n != 0);
+}
